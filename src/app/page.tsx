75b9a2ee2fd162a142de71a1c01cb1fae6af7724
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2025-08-02 13:05:58 +1000

    E2B Sandbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-    return <div>Home</div>;
-}
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+
+const Page = () => {
+    const [value, setValue] = useState("");
+    const trpc = useTRPC();
+    const invoke = useMutation(trpc.invoke.mutationOptions({}));
+    return (
+        <div className="p-4 max-w-7xl mx-auto">
+            <Input value={value} onChange={(e) => setValue(e.target.value)} />
+            <Button
+                disabled={invoke.isPending}
+                onClick={() => invoke.mutate({ value: value })}
+            >
+                Inovke Job
+            </Button>
+        </div>
+    );
+};
+
+export default Page;
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,21 +1,33 @@
 import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
 
-    async ({ event }) => {
+    async ({ event, step }) => {
+        const sandboxId = await step.run("get-sandbox-id", async () => {
+            const sandbox = await Sandbox.create("vibe-nextjs-testfk003");
+            return sandbox.sandboxId;
+        });
         const codeAgent = createAgent({
             name: "code-agent",
-            system: "You are an expert next.js developer. You write readable code.",
+            system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippet",
             model: openai({ model: "gpt-4o" }),
         });
 
         const { output } = await codeAgent.run(
-            `Write the following : ${event.data.value}`,
+            `Write the following snippet : ${event.data.value}`,
         );
 
-        return { output };
+        const sandboxUrl = await step.run("get-sandbox-url", async () => {
+            const sandbox = await getSandbox(sandboxId);
+            const host = sandbox.getHost(3000);
+            return `https://${host}`;
+        });
+
+        return { output, sandboxUrl };
     },
 );
