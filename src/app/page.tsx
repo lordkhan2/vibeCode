c4b8accb4367ba4caf50889bed83786bc75dce8c
Mohammed Farhan Khan <70976558+lordkhan2@users.noreply.github.com>
2025-08-03 10:25:01 +1000

    Messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "sonner";
 
 const Page = () => {
     const [value, setValue] = useState("");
+
     const trpc = useTRPC();
-    const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+    const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
+    const createdMessage = useMutation(
+        trpc.messages.create.mutationOptions({
+            onSuccess: () => {
+                toast.success("Message created");
+            },
+        }),
+    );
+    console.log(messages);
     return (
         <div className="p-4 max-w-7xl mx-auto">
             <Input value={value} onChange={(e) => setValue(e.target.value)} />
             <Button
-                disabled={invoke.isPending}
-                onClick={() => invoke.mutate({ value: value })}
+                disabled={createdMessage.isPending}
+                onClick={() => createdMessage.mutate({ value: value })}
             >
                 Inovke Job
             </Button>
+            {JSON.stringify(messages, null, 2)}
         </div>
     );
 };
